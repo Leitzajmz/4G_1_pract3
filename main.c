a0d42743bfc75f4fb610bdd091bf4f148bea7f5c
Eduardo <egarcia24@ucol.mx>
2018-04-30 00:21:44 -0500

    lectura del caracter si hay algun dato en el buffer

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <18F4620.h>
+#include <STDIO.H>
+#include <stdlib.h>
 #fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=16000000)
+#use RS232(BAUD=9600, XMIT= PIN_C6, rcv=PIN_C7, BITS=8,PARITY=N, STOP=1)
 #use fast_io(a)
 #use fast_io(b)
 #use fast_io(c)
@@ -11,7 +14,8 @@
 
 void main (void){
    setup_oscillator(OSC_16MHZ);
-
+   char dato[15];
+   int contCaracter=0x00;
    while(1){
       if(kbhit()){
           dato[contCaracter]=getch();
